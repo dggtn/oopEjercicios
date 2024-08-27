c58567b8d8acad788ced492899c98ead696e379b
daniela <dggtn97@gmail.com>
2024-08-27 20:36:48 -0300

    ejercicio 7

---
+++ src/Ejercicio7/MainEjercicio7.java
@@ -0,0 +1,5 @@
+package Ejercicio7;
+public class MainEjercicio7 {
+    public static void main(String[] args) {
+        TarjetaDeViaje sube = new TarjetaDeViaje(0);
+    }};
---
+++ src/Ejercicio7/TarjetaDeViaje.java
@@ -0,0 +1,65 @@
+package Ejercicio7;
+
+public class TarjetaDeViaje {
+    private double saldo;
+    private double saldoNegativo = -200;
+    private double saldoPositivo = 1000;
+    private int cantViajes = 0;
+    private double viajeMinimo;
+    private double viajeMaximo;
+    public TarjetaDeViaje(double saldo) {
+        this.saldo = saldo;
+
+    }
+
+    public boolean cargar(double monto) {
+        if (saldo <= saldoPositivo && saldo >= saldoNegativo) {
+            saldo += monto;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean viajar(double precio) {
+        double saldoRestante = saldo - precio;
+        if (saldoRestante >= saldoNegativo) {
+            saldo=saldoRestante;
+            cantViajes += 1;
+            if(cantViajes==1){
+                viajeMaximo=precio;
+                viajeMinimo=precio;
+            } else {
+                if(precio<viajeMinimo){
+                    viajeMinimo=precio;
+                }
+                if(precio>viajeMaximo){
+                    viajeMaximo=precio;
+                }
+            }
+            return true;
+
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+
+
+
+    public int obtenerViajesRealizados() {
+        return cantViajes;
+
+    }
+
+    public double menorPrecio() {
+        return viajeMinimo;
+    };
+
+    public double precioMaximo(){
+        return viajeMaximo;
+    }
+};
