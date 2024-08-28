3ce6639cbf8be27ac2c94e5bb35e54b8220e34f9
daniela <dggtn97@gmail.com>
2024-08-28 09:42:42 -0300

    ejercicio 7 main

--- src/Ejercicio7/MainEjercicio7.java
+++ src/Ejercicio7/MainEjercicio7.java
@@ -2,4 +2,14 @@ package Ejercicio7;
 public class MainEjercicio7 {
     public static void main(String[] args) {
         TarjetaDeViaje sube = new TarjetaDeViaje(0);
-    }};
+
+        System.out.println(sube.cargar(500.00));
+        System.out.println(sube.viajar(400.00));
+        System.out.println(sube.viajar(100.00));
+        System.out.println(sube.viajar(200.00));
+        System.out.println(sube.viajar(200.00));
+        System.out.println(sube.cargar(1000.00));
+        System.out.println(sube.menorPrecio());
+        System.out.println(sube.precioMaximo());
+    }
+};
