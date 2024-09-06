53d38e2476f638cbbe8a50f4ad7b4e3c84be178c
daniela <dggtn97@gmail.com>
2024-09-06 14:49:04 -0300

    uso de for en java

--- src/youtube/Caja.java
+++ src/youtube/Caja.java
@@ -3,6 +3,21 @@ package youtube;
 import java.util.ArrayList;
 
 public class Caja {
-    int numero;
-    ArrayList<Venta>ventas= new ArrayList<Venta>();
+    private int numero;
+    private ArrayList<Venta>ventas= new ArrayList<Venta>();
+
+    public Caja(int numero){
+        this.numero = numero;
+
+    }
+    public void agregarVenta(Venta v){
+        ventas.add(v);
+
+    }
+    public void mostrarVentas(){
+        for (int i = 0; i < ventas.size(); i++) {
+            System.out.println(ventas.get(i));
+
+        }
+    }
 }
