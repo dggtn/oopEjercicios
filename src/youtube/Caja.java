67c669361c7d113b7f57c2301dde6df89496eee7
daniela <dggtn97@gmail.com>
2024-09-06 15:23:06 -0300

    ejemplo de composicion,agregacion

--- src/youtube/Caja.java
+++ src/youtube/Caja.java
@@ -15,10 +15,13 @@ public class Caja {
 
     }
     public void mostrar(){
+        double total = 0;
         for (int i = 0; i < ventas.size(); i++) {
+            total +=ventas.get(1).getMonto();
             ventas.get(i).imprimir();
             System.out.println();
 
         }
+        System.out.println("Total de ventas del dia: "+ total);
     }
 }
