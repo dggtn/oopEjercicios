ffd50f442f5b0fed6e983b46ee00ba7908edd0fa
daniela <dggtn97@gmail.com>
2024-09-07 11:43:08 -0300

    ejercicio facturas

--- src/Ejercicio11/MainEjercicio11.java
+++ src/Ejercicio11/MainEjercicio11.java
@@ -2,8 +2,17 @@ package Ejercicio11;
 
 public class MainEjercicio11 {
     public static void main(String[] args) {
-       Producto prod1 = new Producto(1,"Cocacola 3lts retornable",10);
-        LineaVenta linea = new LineaVenta(3,prod1);
-        System.out.println(linea);
+        Producto prod1 = new Producto(1,"Cocacola 3lts retornable",150);
+        Producto prod2 = new Producto(2,"Cocacola 2lts retornable",100);
+        Producto prod3 = new Producto(3,"Cocacola 1lt retornable",80);
+
+        Venta v = new Venta(1);
+        v.agregar(prod1,3);
+        v.agregar(prod1,5);
+
+        Venta v2 = new Venta(2);
+        v2.agregar(prod1,1);
+        v2.agregar(prod1,5);
+        v2.mostrarTicketventa();
     }
 };
---
+++ src/Ejercicio11/Venta.java
@@ -0,0 +1,34 @@
+package Ejercicio11;
+
+public class Venta {
+    int numeroFactura;
+    Fecha fecha = new Fecha();
+    java.util.ArrayList<LineaVenta>lineas = new java.util.ArrayList<LineaVenta>();
+    public Venta(int numeroFactura1){
+        numeroFactura=numeroFactura1;
+    }
+
+    public void agregar(Producto p,int cantidad){
+        LineaVenta linea = new LineaVenta(cantidad,p);
+        lineas.add(linea);
+
+    }
+    public void mostrarTicketventa(){
+        System.out.println("Numero Fcatura "+ numeroFactura);
+        System.out.println("Fecha: "+fecha);
+        System.out.println("Ca\tDesc\tUni\t\tSubt");
+        for (int i = 0; i < lineas.size(); i++) {
+            System.out.println(lineas.get(i));
+        }
+        System.out.println();
+        System.out.println("Total de la venta: "+calcularTotal());
+        System.out.println();
+    }
+    public double calcularTotal(){
+        double total =0;
+        for (LineaVenta linea : lineas){
+            total +=linea.calcularSubTotal();
+        }
+        return total;
+    }
+};
