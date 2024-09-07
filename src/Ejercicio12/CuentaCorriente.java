3f18353b811773af2f9246d4a92dbde53a9d5ba6
daniela <dggtn97@gmail.com>
2024-09-07 12:47:02 -0300

    clase cuenta corriente

--- src/Ejercicio12/CuentaCorriente.java
+++ src/Ejercicio12/CuentaCorriente.java
@@ -1,4 +1,27 @@
 package Ejercicio12;
 
-public class CuentaCorriente {
+public class CuentaCorriente extends Cuenta{
+    private int numeroCheque=100;
+
+
+    @Override
+    public String toString() {
+        return super.toString() +"CuentaCorriente" +
+                "numeroCheque=" + numeroCheque;
+    }
+
+    public CuentaCorriente() {
+    }
+
+    public CuentaCorriente(String propietario, double saldo) {
+        super(propietario, saldo);
+    }
+
+    public int getNumeroCheque() {
+        return numeroCheque;
+    }
+
+    public void setNumeroCheque(int numeroCheque) {
+        this.numeroCheque = numeroCheque;
+    }
 }
